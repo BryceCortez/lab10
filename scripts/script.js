dc1f39e1ec8f5da9c2f859eac2a24d288d54e71f
Bryce <brycecortez1337@gmil.com>
2022-11-08 15:15:19 -0800

    do/while

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+// for (let i = 0; i < 1000000; i++){
+//     console.log(i)
+// }
+
+// for (let i = 1 ; i <= 10 ; i++){
+//     console.log(2**i)
+// }
+let input;
+do{
+input = prompt ("what is your name");
+//promt always returns string
+//convert input to number
+console.log(Number(input));
+} while(!isNaN(Number(input)));
+
+alert("Hello" + input)
+alert(`Hello ${input}`)
