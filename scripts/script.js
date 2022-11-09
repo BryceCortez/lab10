1d32273b97468a8054edd081553e73c7319c30be
Bryce <brycecortez1337@gmil.com>
2022-11-08 16:00:18 -0800

    added stuff

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,36 @@
 // for (let i = 1 ; i <= 10 ; i++){
 //     console.log(2**i)
 // }
-let input;
-do{
-input = prompt ("what is your name");
-//promt always returns string
-//convert input to number
-console.log(Number(input));
-} while(!isNaN(Number(input)));
+// let input;
+// do{
+// input = prompt ("what is your name");
+// //promt always returns string
+// //convert input to number
+// console.log(Number(input));
+// } while(!isNaN(Number(input)));
 
-alert("Hello" + input);
-alert(`Hello ${input}`);
+// alert("Hello" + input);
+// alert(`Hello ${input}`);
 
-let fruits = ['Apple', 'Banana']
-fruits.push = ['Orange'];
-fruits.unshift = ['Orange'];
-console.log(fruits);
+// let fruits = ['Apple', 'Banana']
+// fruits.push = ['Orange'];
+// fruits.unshift = ['Mango'];
+// console.log(fruits);
 
+// fruits.shift
+// fruits.pop
 
-fruits.pop
+const person = {
+    name:['Bob', 'Smith'],
+    age:32,
+    gender:'male',
+    hobbies :['music', 'skiing']
+}
+
+console.log(`My name is ${person.name[0]} ${person.name[1]}.I am interested in ${person.hobbies[0]} and ${person.hobbies[1]}`)
+
+for (let key in person){
+    console.log(key);
+    console.log(person[key]);
+}
+
